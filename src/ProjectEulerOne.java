0537bfb79881af0ef7c1755cff9f27f24540493c
Eric Wu <eric.ics211@gmail.com>
2015-02-01 17:11:44 -1000

    Answer to ProjectEulerOne

---
+++ src/ProjectEulerOne.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Eric on 2/1/2015.
+ */
+public class ProjectEulerOne {
+  public static void main(String[] args) {
+    double sum = 0;
+    for (int i = 0; i < 1000; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        sum += i;
+      }
+    }
+    System.out.println(sum);
+  }
+}
